69cdd62f87e09bd4133698f9798f443a65d82e70
Patrick Poopathi <patrickgp1992@hotmail.com>
2022-06-18 09:45:30 -0400

    User is able to cycle through questions and will be able to select an answer via getNewQuestion()

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,7 +2,7 @@ const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
 
 let currentQuestion = {};
-let acceptingAnswers = true;
+let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
@@ -42,7 +42,39 @@ startGame = () => {
     questionCounter = 0;
     score = 0;
     availableQuestions = [...questions];
-    
+    getNewQuestion();
 }
+
+getNewQuestion = () => {
+    if(questionCounter >= maxQuestions) {
+        return window.location.assign(".end.html")
+    };
+
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+    
+    choices.forEach( choice => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number]
+    });
+
+    availableQuestions.splice(questionIndex, 1);
+
+    acceptingAnswers = true;
+};
+
+choices.forEach( choice => {
+    choice.addEventListener('click', e =>{
+        if (!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset['number'];
+        getNewQuestion();
+    });
+});
+
 
 startGame();
